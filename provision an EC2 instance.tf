21b54c007d1f78fbb615fd7566ba69e91f80b16e
Esha kashyap <eshakashya18@gmail.com>
2023-04-07 20:30:39 +0530

    Update provision an EC2 instance.tf

--- provision an EC2 instance.tf
+++ provision an EC2 instance.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-06e46074ae430fba6"
   instance_type = "t2.micro"
 
   tags = {
